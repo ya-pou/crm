03e98444eb738d518bfa968c7980845fba284239
WebMate Services <haag.a1994@gmail.com>
2025-11-19 18:08:54 +0100

    feat(users): define entity + main routes

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  // @isUnique({ tableName: 'users', column: 'mail' })
+  email: string;
+
+  @ApiProperty()
+  @IsEnum(['ADMIN', 'MANAGER', 'USER'])
+  profil: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
